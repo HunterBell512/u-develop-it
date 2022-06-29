a04611136865432202c56435c19ea9d541cc664f
Hunter Bell <bellhunter512@gmail.com>
2022-06-29 06:38:32 -0400

    linked the two tables

--- server.js
+++ server.js
@@ -22,42 +22,13 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// query all rows
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
-
-// specific query
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
-});
-
-// Delete a row
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-})
-
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
-
 // get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
     
     db.query(sql, (err, rows) => {
         if (err) {
@@ -73,7 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 // get a single candidate
 app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id 
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
