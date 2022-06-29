97a839a423de2d0f2b8633da80bc2a438dc3bb52
Hunter Bell <bellhunter512@gmail.com>
2022-06-29 05:11:31 -0400

    started setting up database queries

--- server.js
+++ server.js
@@ -21,9 +21,38 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+// query all rows
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 })
+
+// specific query
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete a row
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+})
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 // Default response for any other requests (Not Found)
 app.use((req, res) => {
